31214d90575578e0b746271e01e80d0a4e7ac66e
kaduki-samei <kaduki0331@gmail.com>
2022-03-09 08:13:12 +0000

    [Add]devise新規登録・ログイン

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,7 +1,27 @@
 class Public::UsersController < ApplicationController
+
   def show
   end
 
   def edit
   end
+
+  def quit
+  end
+
+  def out
+    @user = current_user
+    @user.update(is_active: false)
+    reset_session
+    flash[:notice] = "退会しました"
+    redirect_to root_path
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
 end
--- db/migrate/20220305023949_devise_create_users.rb
+++ db/migrate/20220305023949_devise_create_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -32,7 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :name
+      t.string :name, null: false
       t.string :introduction
       t.string :profile_image
       t.boolean :is_active, default: true
@@ -41,7 +41,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    add_index :users, :name,  unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
